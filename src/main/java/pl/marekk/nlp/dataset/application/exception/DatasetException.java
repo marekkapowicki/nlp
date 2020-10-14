d4a53eb36aa06d75197a7ec6ce0fc7f34d5e4bb3
marek.kapowicki <marek.kapowicki@gmail.com>
2020-10-14 11:47:40 +0200

    exception handler

--- src/main/java/pl/marekk/nlp/dataset/application/exception/DatasetException.java
+++
@@ -1,7 +0,0 @@
-package pl.marekk.nlp.dataset.application.exception;
-
-public class DatasetException extends RuntimeException {
-    DatasetException(String message) {
-        super(message);
-    }
-}
--- src/main/java/pl/marekk/nlp/dataset/application/exception/DatasetExceptions.java
+++ src/main/java/pl/marekk/nlp/dataset/application/exception/DatasetExceptions.java
@@ -1,13 +1,15 @@
 package pl.marekk.nlp.dataset.application.exception;
 
 import lombok.experimental.UtilityClass;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.function.Supplier;
 
 @UtilityClass
 public final class DatasetExceptions {
 
-    public static Supplier<DatasetException> notFound(String message) {
-        return () -> new DatasetNotFoundException(message);
+    public static Supplier<RuntimeException> notFound(String message) {
+        return () -> new ResponseStatusException(HttpStatus.NOT_FOUND, message);
     }
 }
